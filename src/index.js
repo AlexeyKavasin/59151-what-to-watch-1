187e05c196d2e3e85a6a94e8fa4da2850ce2d28f
akavasin <azamatdj@gmail.com>
2019-05-03 19:49:28 +0300

    module2-task1: установил prop-types, добавил валидацию props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
+import {Catalog} from "./components/catalog/catalog.jsx";
+
+const names = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
 
 const init = () => {
   ReactDOM.render(
-      <App/>,
+      <App>
+        <Catalog
+          names={names}
+        />
+      </App>,
       document.querySelector(`#root`)
   );
 };
