95ec6b92ba236373ee686cd200f60ee6aa3047dc
kavasin <kavasin@action-media.ru>
2019-06-09 21:43:20 +0300

    module7-task2: временно убрал logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
-import {createLogger} from "redux-logger";
 import {Provider} from "react-redux";
 import combinedReducers from "./redux/reducer/index";
 import {configureAPI} from "./api.js";
@@ -10,11 +9,9 @@ import App from "./components/app/app.jsx";
 import {fetchFilms} from "./redux/reducer/actions.js";
 
 const api = configureAPI((...args) => store.dispatch(...args));
-const logger = createLogger({});
 
 const middlewares = [
-  thunk.withExtraArgument(api),
-  IS_DEV && logger
+  thunk.withExtraArgument(api)
 ];
 
 const store = createStore(
