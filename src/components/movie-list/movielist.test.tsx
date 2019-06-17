dabd52685e1ab0bef75d7663d9581d7c2e0ca817
kavasin <kavasin@action-media.ru>
2019-06-18 00:51:17 +0300

    module9-task1: linter fixes

--- src/components/movie-list/movielist.test.tsx
+++ src/components/movie-list/movielist.test.tsx
@@ -4,14 +4,14 @@ import MovieList from './movielist';
 
 const films = [
   {
-    name: 'Matrix',
-    poster: 'https://es31-server.appspot.com/wtw/static/film/preview/matrix.jpg',
+    name: `Matrix`,
+    poster: `https://es31-server.appspot.com/wtw/static/film/preview/matrix.jpg`,
     id: 0,
-    genre: 'Action',
-    preview_image: 'https://es31-server.appspot.com/wtw/static/film/preview/matrix.jpg',
-    preview_video_link: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm'
+    genre: `Action`,
+    preview_image: `https://es31-server.appspot.com/wtw/static/film/preview/matrix.jpg`,
+    preview_video_link: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
   }
-]
+];
 
 it(`Movie list renders correctly`, () => {
   const tree = renderer
